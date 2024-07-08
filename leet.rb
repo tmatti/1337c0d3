4b28fd2b696be9044564cf9a7987555dc5d66a82
Tim Matti <tmatti56@gmail.com>
2024-07-07 21:12:54 -0400

    rubocop

--- leet.rb
+++ leet.rb
@@ -5,34 +5,34 @@ module Leet
 
   # 88. Merge Sorted Array
   def merge(nums1, m, nums2, n)
-    return if n==0
+    return if n == 0
 
-    i = m + n - 1 
-    while i >= 0 && n > 0 do
-        if m > 0 && nums1[m-1] > nums2[n-1]
-            nums1[i] = nums1[m-1]
-            m-=1
-        else
-            nums1[i] = nums2[n-1]
-            n-=1
-        end 
-        i-=1
+    i = m + n - 1
+    while i >= 0 && n > 0
+      if m > 0 && nums1[m - 1] > nums2[n - 1]
+        nums1[i] = nums1[m - 1]
+        m -= 1
+      else
+        nums1[i] = nums2[n - 1]
+        n -= 1
+      end
+      i -= 1
     end
   end
 
   # 27. Remove Element
   def remove_element(nums, val)
     l = nums.size
-    return 0 if l==0
+    return 0 if l == 0
 
     k = 0
 
     for i in 0...nums.length
-        if nums[i] == val
-            k += 1 
-        else
-            nums[i-k] = nums[i]
-        end
+      if nums[i] == val
+        k += 1
+      else
+        nums[i - k] = nums[i]
+      end
     end
 
     l - k
@@ -40,33 +40,32 @@ module Leet
 
   # 26. Remove Duplicates from Sorted Array
   def remove_duplicates(nums)
-    elems = Hash.new
+    elems = {}
     k = 0
 
-    nums.each_with_index do |num, i|
-        unless elems.key?(num)
-            elems[num] = true
-            nums[k] = num
-            k += 1
-        end
+    nums.each_with_index do |num, _i|
+      next if elems.key?(num)
+
+      elems[num] = true
+      nums[k] = num
+      k += 1
     end
 
     k
   end
 
-
   # 80. Remove Duplicates from Sorted Array II
   def remove_duplicates(nums)
-    elems = Hash.new
+    elems = {}
     elems.default = 0
     k = 0
 
-    nums.each_with_index do |num, i|
-        unless elems[num] >= 2
-            elems[num] += 1
-            nums[k] = num
-            k += 1
-        end
+    nums.each_with_index do |num, _i|
+      next if elems[num] >= 2
+
+      elems[num] += 1
+      nums[k] = num
+      k += 1
     end
 
     k
